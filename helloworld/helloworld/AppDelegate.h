c6691dfbf907542fc0c0226633af24e51668bc4b
Carlos L. (eoncoder) <carlos@eoncoder.com>
2014-03-14 19:56:10 +0100

    Proyecto Base

---
+++ helloworld/helloworld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  helloworld
+//
+//  Created by eoncoder on 14/03/14.
+//  Copyright (c) 2014 eoncoder.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ helloworld/helloworld/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  helloworld
+//
+//  Created by eoncoder on 14/03/14.
+//  Copyright (c) 2014 eoncoder.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
